f0bfd9c8ef970e0c5a53a416a7503c6c7d71d476
Reidemption_ <D00324808@dixie.dmail.edu>
2021-06-24 10:23:16 -0600

    changing port + start script

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const server = require("./server");
 const persist = require("./persist");
 
 // define a port
-const port = process.argv[2] || 8080;
+const port = process.argv[2] || 54828;
 
 persist.onConnect(() => {
-    // start the server
-    server.listen(port, () => {
-        console.log(`Running server on port ${port}`);
-    });
+  // start the server
+  server.listen(port, () => {
+    console.log(`Running server on port ${port}`);
+  });
 });
 
 // connect to the database
 persist.connect();
-
